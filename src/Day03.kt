a2dfbf9409bc3ec7e20f926dc7df35f66f878df7
xxfast <isurukusumal36@gmail.com>
2022-12-03 22:04:13 +1100

    Add day 3 solution

---
+++ src/Day03.kt
@@ -0,0 +1,26 @@
+fun main() {
+
+  fun Sequence<Set<Char>>.scored() = this
+    .map { it.map { char -> if (char.isLowerCase()) char - 'a' else char - 'A' + 26 } + 1 }
+    .flatten()
+    .sum()
+
+  fun part1(input: List<String>) = input.asSequence()
+    .map { rucksack -> rucksack.chunked(rucksack.length / 2) { it.toSet()} }
+    .map { (first, second) -> first intersect second }
+    .scored()
+
+  fun part2(input: List<String>) = input.asSequence()
+    .chunked(3) { group -> group.map { it.toSet() } }
+    .map { (first, second, third) -> first intersect second intersect third }
+    .scored()
+
+  // test if implementation meets criteria from the description, like:
+  val testInput: List<String> = readLines("Day03_test")
+  check(part1(testInput) == 157)
+  check(part2(testInput) == 70)
+
+  val input: List<String> = readLines("Day03")
+  println(part1(input))
+  println(part2(input))
+}
